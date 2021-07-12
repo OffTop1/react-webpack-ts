c3899290d3085219789ad6320c170e4cff714435
Kuyoshbek Mekhmonov <Kuyoshbek_Mekhmonov@epam.com>
2021-07-12 16:38:27 +0500

    feat: split webpack config into dev & prod

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import HelloWorld from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 ReactDOM.render(
     <Provider store={store}>
         <HelloWorld />
--- webpack.common.js
+++ webpack.common.js
@@ -5,13 +5,8 @@ const webpack = require("webpack");
 module.exports = {
     entry: "./src/index.tsx",
     output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
-    mode: process.env.NODE_ENV || "development",
     resolve: { 
         extensions: [".tsx", ".ts", ".js"]
-    },
-    devServer: { 
-        contentBase: path.join(__dirname, "src"),
-        hot: true
     },
     module: {
         rules: [
